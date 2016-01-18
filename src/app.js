7a5ddf8bd0ba4134fafd96dc14964d36043ca3cc
vandosant <sjskender@gmail.com>
2016-01-18 15:38:28 -0700

    Add basic routing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './styles/main.styl'
+import Router from './router'
 
-const Hello = React.createClass({
-  render() {
-    return <div>Hello, {this.props.name}</div>
+window.app = {
+  init() {
+    this.router = new Router()
+    this.router.history.start()
   }
-});
+}
 
-React.render(<Hello name='scott' />, document.body);
+window.app.init()
