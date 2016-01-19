824280d7f70275bc194dc29ceb1f31b456bb27de
Scott Skender <sskender@sskender-mb15.local>
2016-01-19 08:48:06 -0700

    Wrap pages in a layout component

--- src/layout.js
+++ src/layout.js
@@ -1,30 +1,22 @@
 import React from 'react'
-import ampersandMixin from 'ampersand-react-mixin'
-import NavHelper from './components/nav-helper'
 
 export default React.createClass({
-  mixins: [ampersandMixin],
-
-  displayName: 'Layout',
-
-  render () {
-    const {me} = this.props
-
+  render() {
     return (
-      <NavHelper>
+      <div>
         <nav className='top-nav top-nav-light cf' role='navigation'>
           <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
           <label htmlFor='menu-toggle'>Menu</label>
           <ul className='list-unstyled list-inline cf'>
             <li>Labelr</li>
             <li><a href='/repos'>Repos</a></li>
-            <li className='pull-right'><a href='/logout'>Logout</a> {me.login}</li>
+            <li className='pull-right'><a href='/logout'>Logout</a></li>
           </ul>
         </nav>
         <div className='container'>
           {this.props.children}
         </div>
-      </NavHelper>
+      </div>
     )
   }
 })
--- src/router.js
+++ src/router.js
@@ -2,16 +2,23 @@ import Router from 'ampersand-router'
 import React from 'react'
 import Public from './pages/public'
 import Repos from './pages/repos'
+import Layout from './layout'
 
 export default Router.extend({
+  renderPage(page, options = {layout: true}) {
+    if (options.layout) {
+      page = <Layout>{page}</Layout>
+    }
+    React.render(page, document.body)
+  },
   routes: {
     '': 'public',
     'repos': 'repos'
   },
   public() {
-    React.render(<Public />, document.body)
+    this.renderPage(<Public />, {layout: false})
   },
   repos() {
-    React.render(<Repos />, document.body)
+    this.renderPage(<Repos />)
   }
 })
