3412b832d4239069bdc4e0da9f2d41e421e1274b
vandosant <sjskender@gmail.com>
2016-01-19 19:20:59 -0700

    Committing localLinks.js

---
+++ src/localLinks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import localLinks from 'local-links'
+
+class LocalLinks extends React.Component {
+  onClick (event) {
+    const pathName = localLinks.getLocalPathname(event)
+    if (pathName) {
+      event.preventDefault()
+      app.router.history.navigate(pathName)
+    }
+  }
+  render() {
+    return <div onClick={this.onClick} >{this.props.children}</div>
+  }
+}
+
+export default LocalLinks
