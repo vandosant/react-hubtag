98be70617dc317e7ee5dbaf07671c1751afc1824
vandosant <sjskender@gmail.com>
2016-02-06 16:40:20 -0700

    Octoface icon added to layout

--- src/layout.js
+++ src/layout.js
@@ -13,6 +13,7 @@ export default React.createClass({
           <ul className='list-unstyled list-inline cf'>
             <li>Labelr</li>
             <li><a href='/repos'>Repos</a></li>
+            <li className='pull-right'><span className="octicon octicon-octoface"></span></li>
             <li className='pull-right'><a href='/logout'>Logout</a> {me.login}</li>
           </ul>
         </nav>
