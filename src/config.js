fe7ab7cd4f376e7c45f5957bc971291e1b5b6476
vandosant <sjskender@gmail.com>
2016-03-07 13:07:39 -0700

    Mon Mar  7 13:07:39 MST 2016

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const config = {
 
   'vandosant-hubtag.surge.sh': {
     authUrl: 'http://vandosant-gatekeeper.herokuapp.com/authenticate',
-    clientId: 'ffa9424ab602a3fd520a'
+    clientId: '5f69c671afe8d48b4a1f'
   }
 }[window.location.hostname]
 
